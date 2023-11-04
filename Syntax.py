07ac628145f3318c34fe6e6d02ace72972d42572
ahmadhridoy <badaruddinahmadridoy11@gmail.com>
2023-11-04 22:24:41 +0600

    python practice

--- Syntax.py
+++ Syntax.py
@@ -1,2 +1,24 @@
-print("125")
-print(52.3)
+
+
+#python variables
+'''
+name = "Hridoy"
+hri = 123
+ru = 'mu'
+print(name)
+print(hri)
+print(ru)'''
+
+#MULTIple values
+
+x,y,z = "hri",'do','y'
+print(x)
+print(y)
+print(z)
+
+#unpack a list
+fruits = ['apple','mango','banana']
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
