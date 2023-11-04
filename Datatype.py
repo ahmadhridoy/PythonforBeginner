d76e905365657e2db21845e2d5a369e0ceb8e8e2
ahmadhridoy <badaruddinahmadridoy11@gmail.com>
2023-11-05 00:40:24 +0600

    python practice

---
+++ Datatype.py
@@ -0,0 +1,26 @@
+# x = "hsfhsbfjsj"
+# print(type(x))
+# print(x)
+#
+# v =int(20.3)
+# print(type(v))
+# print(v)
+#
+# a = ["s", "d", "r"]
+# print(type(a))
+# print(a)
+#
+# b = ('f', 'p')
+# print(type(b))
+# print(b)
+#
+# g = range(-56)
+# print(g)
+# print(type(g))
+#
+# l = {'name' : 'Hridoy'}
+# print(type(l))
+# print(l)
+
+Name = 'Bodor uddin ahmad ridoy'
+print('My name is' + ' ' + Name)
