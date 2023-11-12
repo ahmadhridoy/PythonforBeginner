c4774b9a1cc00198dde2b918a1a52bd9a78cc870
ahmadhridoy <badaruddinahmadridoy11@gmail.com>
2023-11-12 23:06:48 +0600

    python practice

--- Binarydata.py
+++ Binarydata.py
@@ -1,13 +1,64 @@
-hrilist = [1, 2, 3,4,5]
+# hrilist = [1, 2, 3,4,5]
+#
+# a = bytes(hrilist)
+# print(type(a))
+# print(a)
+#
+# hrilist = [2,6,5,4]
+#
+# b = bytearray(hrilist)
+# print(type(b))
+# b[2]=120
+# print(b)
+# print(b[2])
+'''
+str = input()
+count = 0
+min_val = 10e9
 
-a = bytes(hrilist)
-print(type(a))
-print(a)
+di = {}
 
-hrilist = [2,6,5,4]
+for i in range(len(str)) :
+    if str[i] == 'C' :
+        count -= 1
+    elif str[i] == 'G' :
+        count += 1
 
-b = bytearray(hrilist)
-print(type(b))
-b[2]=120
-print(b)
-print(b[2])
+    di.update({i+1: count})
+    min_val = min(min_val , count)
+
+ans = []
+
+for key, val in di.items() :
+    if min_val == val :
+        ans.append(key)
+
+print(*ans)
+'''
+
+'''str1 = input()
+str2 = input()
+
+count = 0
+for i in range(len(str1)) :
+    if str1[i] != str2[i] :
+        count += 1
+
+print(count)'''
+
+pat = input()
+str = input()
+d = int(input())
+count = 0
+lst = []
+for i in range(len(str) - len(pat) + 1) :
+    sub_str = str[i:i+len(pat)]
+    count = 0
+    for j in range(len(pat)) :
+        if sub_str[j] != pat[j] :
+            count += 1
+    if count <= d :
+     lst.append(i)
+
+
+print(*lst)
---
+++ operator.py
@@ -0,0 +1,9 @@
+'''a = 0
+b = 15
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b)
+print(a // b)'''
